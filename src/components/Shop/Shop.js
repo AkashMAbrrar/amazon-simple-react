0de503d77d300dd7f8db8b5c39e5aec4895993a1
AkashAbrrar <mabrrar9@gmail.com>
2022-03-22 14:19:34 +0600

    this commit is for header

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Shop.css';
+
+const Shop = () => {
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+                <h3>This is for producrs</h3>
+            </div>
+            <div className="card-container">
+                <h4>Order Sumary</h4>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
