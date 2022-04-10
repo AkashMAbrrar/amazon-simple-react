9a300d1d61d111354af083956fdb536934a4aa91
AkashAbrrar <mabrrar9@gmail.com>
2022-04-10 20:30:48 +0600

    commit again for new edition by firebase

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,6 +3,7 @@ import './Cart.css'
 
 const Cart = (props) => {
     const { cart } = props;
+    console.log(props.children);
     let total = 0;
     let shipping = 0;
     let quantity = 0;
@@ -23,6 +24,7 @@ const Cart = (props) => {
             <p>Total Shipping: ${shipping}</p>
             <p>Tax: ${tax}</p>
             <h5>Grand Total: ${grandTotal.toFixed(2)}</h5>
+            {props.children}
         </div>
     );
 };
